019799bde4e2c66b06ba1f0ecc91d68961ba1364
Jan Teske <jan.teske@gmail.com>
2017-10-29 13:14:07 +0100

    Improved lib exports.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ extern crate byteorder;
 
 pub mod file;
 pub mod result;
+pub mod read;
+pub mod write;
 
 mod convert;
 mod headers;
-mod read;
-mod write;
 
+pub use file::File;
 pub use read::{Encoder, Reader};
 pub use write::{Decoder, Writer};
